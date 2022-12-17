658d4f5cc2264634c5695557ec4c86a2d1aad3a6
Suneet Misra <hello@suneet.codes>
2022-12-17 15:38:18 +0000

    chore: adds eslint, prettier and associated configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 function App() {
-
   return (
-<div id="App">
-<h1 className="text-3xl font-bold text-red-500 text-center">
-      Placeholder to test tailwind setup
-    </h1>
-</div>
-  )
+    <div id='App'>
+      <h1 className='text-center text-3xl font-bold text-red-500'>
+        Placeholder to test tailwind setup
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
