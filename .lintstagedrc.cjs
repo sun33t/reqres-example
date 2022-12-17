690d4ddefae722cf2f5b72d6e2f157def67f9e5a
Suneet Misra <hello@suneet.codes>
2022-12-17 15:50:25 +0000

    chore: adds commit linting and git lifecycle hooks for code quality checks

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `yarn eslint --fix ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,json,jsx,ts,tsx,css,md}': 'prettier --write',
+};
---
+++ commitlint.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('@commitlint/types').UserConfig} */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  helpUrl: 'please use `yarn commit` instead of `git commit`',
+};
